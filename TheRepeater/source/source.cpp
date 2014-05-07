ff21ad297c32a4058f280e030998bade060b8a2a
Karan Talreja <karant@noida.atrenta.com>
2014-05-07 12:29:33 +0530

    Accepted code for TheRepeater both large and small test cases

--- TheRepeater/source/source.cpp
+++ TheRepeater/source/source.cpp
@@ -6,6 +6,8 @@
 #include <string>
 #include <set>
 #include <vector>
+#include <algorithm>
+#include <math.h>
 using namespace std;
 #define getcx getchar_unlocked
 inline void inpLine(char *str)
@@ -33,105 +35,84 @@ inline void inp( int &n )//fast input function
 }
 int main()
 {
-	int noOfTestCases = 0;
-	int noOfStrings =0;
-	char* buffer = (char*)malloc(sizeof(char)*100);
-	inp(noOfTestCases);
-	for(int i =0;i < noOfTestCases ; ++i)
-	{
-		set<string> allStringsHash;
-		vector<string> allStrings;
-		set<string> hash;
-		inp(noOfStrings);
-		string buff;
-		for (int j =0;j<noOfStrings;j++)
-		{
-			inpLine(buffer);
-//			cout<<buffer<<" : ";
-			buff.clear();
-			char prev = buffer[0];
-			buff += buffer[0];
-			int bufferLen = strlen(buffer);
-			for(int k = 1;k < bufferLen;k++)
-			{
-				if(buffer[k] == prev)
-					continue;
-				else
-				{
-					buff += buffer[k];
-					prev = buffer[k];
-				}
-			}
-//			cout<< buff.c_str()<<endl;
-			hash.insert(buff);
-			allStringsHash.insert(string(buffer));
-			allStrings.push_back(string(buffer));
-		}
-		if(hash.size() > 1)
-		{
-			cout<<"Case #"<<i+1<<": Fegla Won"<<endl;
-			continue;
-		}
-		bool notInSrc = false;
-		if(allStringsHash.find(buff) == allStringsHash.end())
-			notInSrc = true;
-		allStringsHash.insert(buff);
-		allStrings.push_back(buff);
-		int globalMin = 10000000;
-		vector<string>::iterator itrTar = allStrings.begin();
-		for (; itrTar != allStrings.end(); ++itrTar)
-		{
-			int localMin =0;
-			vector<int> posOfStart;
-			const char* targetStr = (itrTar)->c_str();
-			char prev = targetStr[0];
-			int diff = 0;
-			for (int m =0; m < strlen(targetStr);m++)
-			{
-				if(targetStr[m] == prev)
-					continue;
-				else
-				{
-					prev = targetStr[m];
-					posOfStart.push_back(m - diff);
-					diff = m;
-				}
-			}
-			posOfStart.push_back(strlen(targetStr) - diff);
-			vector<string>::iterator itrSrc = allStrings.begin();
-			for (;  itrSrc != allStrings.end(); ++itrSrc)
-			{
-				vector<int> posOfEnd;
+    int noOfTestCases = 0;
+    int noOfStrings =0;
+    char* buffer = (char*)malloc(sizeof(char)*100);
+    inp(noOfTestCases);
+    for(int i =0;i < noOfTestCases ; ++i)
+    {
+        vector<string> allStrings;
+        set<string> hash;
+        vector<vector<int > > LettersInString;
+        inp(noOfStrings);
+        string buff;
+        for (int j =0;j<noOfStrings;j++)
+        {
+            inpLine(buffer);
+//            cout<<buffer<<" : ";
+            buff.clear();
+            char prev = buffer[0];
+            buff += buffer[0];
+            int bufferLen = strlen(buffer);
+            for(int k = 1;k < bufferLen;k++)
+            {
+                if(buffer[k] == prev)
+                    continue;
+                else
+                {
+                    buff += buffer[k];
+                    prev = buffer[k];
+                }
+            }
+            //			cout<< buff.c_str()<<endl;
+            hash.insert(buff);
+            allStrings.push_back(string(buffer));
+            LettersInString.push_back(vector<int> (buff.size(),1));
+            prev = buffer[0];
+            bufferLen = strlen(buffer);
+            int idxInArray =0;
+            for(int k = 1;k < bufferLen;k++)
+            {
+                if(buffer[k] == prev)
+                {
+                    LettersInString[j][idxInArray]++;
+                    continue;
+                }
+                else
+                {
+                    prev = buffer[k];
+                    idxInArray++;
+                }
+            }
+        }
+        if(hash.size() > 1)
+        {
+            cout<<"Case #"<<i+1<<": Fegla Won"<<endl;
+            continue;
+        }
+        size_t globalMin = 0;
+        size_t max = LettersInString[0].size();
+        size_t answer = 0;
+        for (size_t k = 0; k< max ;k++)
+        {
+            vector<int > tempvec;
+            for (size_t j = 0 ; j< LettersInString.size(); j++)
+                tempvec.push_back(LettersInString[j][k]);
 
-				const char* sourceStr = (itrSrc)->c_str();
-				if (strcmp (sourceStr,targetStr) == 0) continue;
-				if (notInSrc == true && strcmp (sourceStr, buff.c_str()) == 0) continue;
-				char prev = sourceStr[0];
-				int diff = 0;
-				for (int m =0; m < strlen(sourceStr);m++)
-				{
-					if(sourceStr[m] == prev)
-						continue;
-					else
-					{
-						prev = sourceStr[m];
-						posOfEnd.push_back(m - diff);
-						diff = m;
-					}
-				}
-				posOfEnd.push_back(strlen(sourceStr) - diff);
-//				cout<< targetStr << " : " << sourceStr <<endl;
-				for (int m =0; m < posOfEnd.size();m++)
-				{
-//					cout<< posOfStart[m] << " : " << posOfEnd[m];
-					localMin += abs(posOfStart[m] - posOfEnd[m]);
-//					cout<< ":" << localMin <<endl;
-				}
-			}
-			if(globalMin > localMin)
-				globalMin = localMin;
-		}
-		cout<<"Case #"<<i+1<<": "<<globalMin<<endl;
-	}
-	return 0;
+            sort(tempvec.begin(), tempvec.end(),less<int>());
+            int median = 0;
+
+            if(tempvec.size() % 2 == 0)
+                median = (tempvec[tempvec.size()/2 -1] + tempvec[tempvec.size()/2]) / 2;
+            else
+                median = tempvec[(tempvec.size()+1)/2 -1];
+
+            for (size_t j = 0 ; j< LettersInString.size(); j++)
+                answer += abs(LettersInString[j][k] - median);
+        }
+
+
+        cout<<"Case #"<<i+1<<": "<<answer<<endl;
+    }
+    return 0;
 }
